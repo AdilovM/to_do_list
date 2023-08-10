5e53c337335d6fbe87814d55270ac9e642712d13
adilovm <miras9191@gmail.com>
2023-08-10 14:52:17 -0700

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Miras Adilov on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
